bbb36868fcdd08ac0ab97bbf5b49922535a446f4
KitYoonJeongmin <102850131+KitYoonJeongmin@users.noreply.github.com>
2022-08-22 10:13:26 +0900

    [update] AI 제작 완료

---
+++ Source/ArenaBattle/ABGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ABGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API AABGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
